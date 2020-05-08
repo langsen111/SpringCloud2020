ffc2ae8650571c05b3a37ba55d00677c95c517cd
langsen111 <aiyslm@foxmail.com>
2020-05-08 18:13:03 +0800

    add Sleuth and Zipkin

--- cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -72,4 +72,10 @@ public class PaymentController {
         }
         return serverProt;
     }
+
+    @GetMapping("/payment/zipkin")
+    public String paymentZipkin(){
+
+        return "hi, i'am paymentzipkin server fall back, welcome to atguigu, 😄～～";
+    }
 }
