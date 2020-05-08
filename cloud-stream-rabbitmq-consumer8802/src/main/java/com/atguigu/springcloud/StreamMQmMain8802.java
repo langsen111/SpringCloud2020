b04fa771dfa53f161890e629e4966e9096378885
langsen111 <aiyslm@foxmail.com>
2020-05-08 16:56:37 +0800

    add rabbitmq

---
+++ cloud-stream-rabbitmq-consumer8802/src/main/java/com/atguigu/springcloud/StreamMQmMain8802.java
@@ -0,0 +1,16 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author harry
+ * @create 2020-05-07 17:56
+ * @Version 1.0
+ */
+@SpringBootApplication
+public class StreamMQmMain8802 {
+    public static void main(String[] args) {
+        SpringApplication.run(StreamMQmMain8802.class,args);
+    }
+}
---
+++ cloud-stream-rabbitmq-consumer8803/src/main/java/com/atguigu/springcloud/StreamMQMain8803.java
@@ -0,0 +1,16 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author harry
+ * @create 2020-05-07 18:29
+ * @Version 1.0
+ */
+@SpringBootApplication
+public class StreamMQMain8803 {
+    public static void main(String[] args) {
+        SpringApplication.run(StreamMQMain8803.class,args);
+    }
+}
